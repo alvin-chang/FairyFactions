3d387941b207e1f22352a21227f485f1f823887a
BudVar <superbudvar@gmail.com>
2020-03-29 06:22:22 +0100

    updates to model

--- src/main/java/fairies/client/render/ModelFairy.java
+++ src/main/java/fairies/client/render/ModelFairy.java
@@ -3,47 +3,54 @@ package fairies.client.render;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 import org.lwjgl.opengl.GL11;
 
-public class ModelFairy extends ModelBiped {
-  public ModelFairy() { this(0.0F); }
+import fairies.FairyFactions;
 
-  public ModelFairy(final float f) { this(f, 0.0F); }
+@SideOnly(Side.CLIENT)
+public class ModelFairy extends ModelBiped {
+  public ModelFairy() {
+    this(0.0F);
+  }
+
+  public ModelFairy(final float modelSize) {
+    this(modelSize, 0.0F);
+  }
 
   public ModelFairy(final float f, final float f1) {
-    leftArmPose = ArmPose.EMPTY;
-    rightArmPose = ArmPose.EMPTY;
+    this.leftArmPose = ArmPose.EMPTY;
+    this.rightArmPose = ArmPose.EMPTY;
     isSneak = false;
     flymode = showCrown = false;
     bipedHead = new ModelRenderer(this, 0, 0);
     bipedHead.addBox(-3F, -6F, -3F, 6, 6, 6, f);
     bipedHead.setRotationPoint(0.0F, 0.0F + f1, 0.0F);
-    strand = (new ModelRenderer(this)).setTextureSize((byte)64, (byte)32);
+    strand = (new ModelRenderer(this)).setTextureSize((byte) 64, (byte) 32);
     strand.setTextureOffset(0, 20).addBox(-3F, -5F, 3F, 6, 3, 1, f);
     strand.setTextureOffset(24, 0).addBox(-4F, -5F, -3F, 1, 3, 6, f);
     strand.setTextureOffset(24, 0).addBox(3F, -5F, -3F, 1, 3, 6, f);
     strand.setRotationPoint(0.0F, 0.0F + f1, 0.0F);
-    strand2 = (new ModelRenderer(this)).setTextureSize((byte)64, (byte)32);
+    strand2 = (new ModelRenderer(this)).setTextureSize((byte) 64, (byte) 32);
     strand2.setTextureOffset(13, 23).addBox(-5F, -2.5F, 1.5F, 10, 3, 3, f);
     strand2.setRotationPoint(0F, 0F + f1, 0F);
     strand.addChild(strand2);
-    strand3 = (new ModelRenderer(this)).setTextureSize((byte)64, (byte)32);
-    strand3.setTextureOffset(13, 23).addBox(-3F, -1.5F, -1.5F, 3, 3, 3,
-                                            f - 0.5F);
-    strand3.setTextureOffset(13, 23).addBox(-5.25F, -1.5F, -1.5F, 3, 3, 3,
-                                            f - 0.25F);
+    strand3 = (new ModelRenderer(this)).setTextureSize((byte) 64, (byte) 32);
+    strand3.setTextureOffset(13, 23).addBox(-3F, -1.5F, -1.5F, 3, 3, 3, f - 0.5F);
+    strand3.setTextureOffset(13, 23).addBox(-5.25F, -1.5F, -1.5F, 3, 3, 3, f - 0.25F);
     strand3.setRotationPoint(-2F, -1.75F + f1, 3F);
     strand3.rotateAngleZ = -1.0F;
     strand3.rotateAngleY = 0.5F;
     strand3.isHidden = true;
     strand.addChild(strand3);
-    strand4 = (new ModelRenderer(this)).setTextureSize((byte)64, (byte)32);
+    strand4 = (new ModelRenderer(this)).setTextureSize((byte) 64, (byte) 32);
     strand4.mirror = true;
-    strand4.setTextureOffset(13, 23).addBox(0F, -1.5F, -1.5F, 3, 3, 3,
-                                            f - 0.5F);
-    strand4.setTextureOffset(13, 23).addBox(2.25F, -1.5F, -1.5F, 3, 3, 3,
-                                            f - 0.25F);
+    strand4.setTextureOffset(13, 23).addBox(0F, -1.5F, -1.5F, 3, 3, 3, f - 0.5F);
+    strand4.setTextureOffset(13, 23).addBox(2.25F, -1.5F, -1.5F, 3, 3, 3, f - 0.25F);
     strand4.setRotationPoint(2F, -1.75F + f1, 3F);
     strand4.rotateAngleZ = 1.0F;
     strand4.rotateAngleY = -0.5F;
@@ -56,7 +63,7 @@ public class ModelFairy extends ModelBiped {
     crown = new ModelRenderer(this, 37, 14);
     crown.addBox(-3F, -6.75F, -3F, 6, 3, 6, f + 0.25F);
     crown.setRotationPoint(0.0F, 0.0F + f1, 0.0F);
-    bipedBody = (new ModelRenderer(this)).setTextureSize((byte)64, (byte)32);
+    bipedBody = (new ModelRenderer(this)).setTextureSize((byte) 64, (byte) 32);
     bipedBody.setTextureOffset(8, 12).addBox(-2F, 0.0F, -1F, 4, 6, 2, f);
     bipedBody.setTextureOffset(15, 20).addBox(-2F, 1.0F, -2F, 4, 2, 1, f);
     bipedBody.setRotationPoint(0.0F, 0.0F + f1, 0.0F);
@@ -84,55 +91,51 @@ public class ModelFairy extends ModelBiped {
   }
 
   @Override
-  public void render(final Entity e, final float f, final float f1,
-                     final float f2, final float f3, final float f4,
-                     final float f5) {
+  public void render(final Entity entityIn, final float limbSwing, final float limbSwingAmount, final float ageInTicks,
+      final float netHeadYaw, final float headPitch, final float scale) {
     GL11.glPushMatrix();
-    setRotationAngles(f, f1, f2, f3, f4, f5);
-    bipedHead.render(f5);
-    bipedBody.render(f5);
-    bipedRightArm.render(f5);
-    bipedLeftArm.render(f5);
-    bipedRightLeg.render(f5);
-    bipedLeftLeg.render(f5);
+    setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+    bipedHead.render(scale);
+    bipedBody.render(scale);
+    bipedRightArm.render(scale);
+    bipedLeftArm.render(scale);
+    bipedRightLeg.render(scale);
+    bipedLeftLeg.render(scale);
 
     if (!rogueParts) {
       strand2.isHidden = hairType;
       strand3.isHidden = !hairType;
       strand4.isHidden = !hairType;
-      strand.render(f5);
+      strand.render(scale);
     }
 
     if (showCrown && !rogueParts) {
-      crown.render(f5);
+      crown.render(scale);
     }
 
     if (!scoutWings && !rogueParts) {
-      wingLeft.render(f5);
-      wingRight.render(f5);
+      wingLeft.render(scale);
+      wingRight.render(scale);
     }
 
     GL11.glPopMatrix();
   }
 
-  public void setRotationAngles(final float f, final float f1, final float f2,
-                                final float f3, final float f4,
-                                final float f5) {
-    bipedHead.rotateAngleY = f3 / (180F / (float)Math.PI);
-    bipedHead.rotateAngleX = f4 / (180F / (float)Math.PI);
+  public void setRotationAngles(final float limbSwing, final float limbSwingAmount, final float f2, final float f3,
+      final float f4, final float f5) {
+    bipedHead.rotateAngleY = f3 / (180F / (float) Math.PI);
+    bipedHead.rotateAngleX = f4 / (180F / (float) Math.PI);
     strand.rotateAngleY = bipedHead.rotateAngleY;
     strand.rotateAngleX = bipedHead.rotateAngleX;
     crown.rotateAngleY = bipedHead.rotateAngleY;
     crown.rotateAngleX = bipedHead.rotateAngleX;
 
     if (!flymode) {
-      bipedRightArm.rotateAngleX =
-          MathHelper.cos(f * 0.6662F + (float)Math.PI) * 2.0F * f1 * 0.5F;
-      bipedLeftArm.rotateAngleX =
-          MathHelper.cos(f * 0.6662F) * 2.0F * f1 * 0.5F;
-      bipedRightLeg.rotateAngleX = MathHelper.cos(f * 0.6662F) * 1.4F * f1;
-      bipedLeftLeg.rotateAngleX =
-          MathHelper.cos(f * 0.6662F + (float)Math.PI) * 1.4F * f1;
+      bipedRightArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * 2.0F * limbSwingAmount
+          * 0.5F;
+      bipedLeftArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F;
+      bipedRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+      bipedLeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
     } else {
       bipedRightArm.rotateAngleX = 0.0F;
       bipedLeftArm.rotateAngleX = 0.0F;
@@ -148,27 +151,25 @@ public class ModelFairy extends ModelBiped {
     bipedLeftLeg.rotateAngleZ = 0.0F;
 
     if ((isRiding || isSneak) && !flymode) {
-      bipedRightArm.rotateAngleX += -((float)Math.PI / 5F);
-      bipedLeftArm.rotateAngleX += -((float)Math.PI / 5F);
-      bipedRightLeg.rotateAngleX = -((float)Math.PI * 2F / 5F);
-      bipedLeftLeg.rotateAngleX = -((float)Math.PI * 2F / 5F);
-      bipedRightLeg.rotateAngleY = ((float)Math.PI / 10F);
-      bipedLeftLeg.rotateAngleY = -((float)Math.PI / 10F);
+      bipedRightArm.rotateAngleX += -((float) Math.PI / 5F);
+      bipedLeftArm.rotateAngleX += -((float) Math.PI / 5F);
+      bipedRightLeg.rotateAngleX = -((float) Math.PI * 2F / 5F);
+      bipedLeftLeg.rotateAngleX = -((float) Math.PI * 2F / 5F);
+      bipedRightLeg.rotateAngleY = ((float) Math.PI / 10F);
+      bipedLeftLeg.rotateAngleY = -((float) Math.PI / 10F);
 
       if (isSneak) {
-        bipedRightLeg.rotateAngleX = -((float)Math.PI / 2F);
-        bipedLeftLeg.rotateAngleX = -((float)Math.PI / 2F);
+        bipedRightLeg.rotateAngleX = -((float) Math.PI / 2F);
+        bipedLeftLeg.rotateAngleX = -((float) Math.PI / 2F);
       }
     }
 
-    if (leftArmPose != ArmPose.EMPTY) {
-      bipedLeftArm.rotateAngleX =
-          bipedLeftArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
+    if (this.leftArmPose != ArmPose.EMPTY) {
+      bipedLeftArm.rotateAngleX = bipedLeftArm.rotateAngleX * 0.5F - ((float) Math.PI / 10F);
     }
 
-    if (rightArmPose != ArmPose.EMPTY) {
-      bipedRightArm.rotateAngleX =
-          bipedRightArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
+    if (this.rightArmPose != ArmPose.EMPTY) {
+      bipedRightArm.rotateAngleX = bipedRightArm.rotateAngleX * 0.5F - ((float) Math.PI / 10F);
     }
 
     bipedRightArm.rotateAngleY = 0.0F;
@@ -176,20 +177,13 @@ public class ModelFairy extends ModelBiped {
 
     if (swingProgress > -9990F) {
       float f6 = swingProgress;
-      bipedBody.rotateAngleY =
-          MathHelper.sin(MathHelper.sqrt(f6) * (float)Math.PI * 2.0F) *
-          0.2F;
-      wingLeft.rotateAngleY = wingRight.rotateAngleY =
-          MathHelper.sin(MathHelper.sqrt(f6) * (float)Math.PI * 2.0F) *
-          0.2F;
-      bipedRightArm.rotationPointZ =
-          MathHelper.sin(bipedBody.rotateAngleY) * 5F;
-      bipedRightArm.rotationPointX =
-          -MathHelper.cos(bipedBody.rotateAngleY) * 5F + 2.0F;
-      bipedLeftArm.rotationPointZ =
-          -MathHelper.sin(bipedBody.rotateAngleY) * 5F;
-      bipedLeftArm.rotationPointX =
-          MathHelper.cos(bipedBody.rotateAngleY) * 5F - 2.0F;
+      bipedBody.rotateAngleY = MathHelper.sin(MathHelper.sqrt(f6) * (float) Math.PI * 2.0F) * 0.2F;
+      wingLeft.rotateAngleY = wingRight.rotateAngleY = MathHelper.sin(MathHelper.sqrt(f6) * (float) Math.PI * 2.0F)
+          * 0.2F;
+      bipedRightArm.rotationPointZ = MathHelper.sin(bipedBody.rotateAngleY) * 5F;
+      bipedRightArm.rotationPointX = -MathHelper.cos(bipedBody.rotateAngleY) * 5F + 2.0F;
+      bipedLeftArm.rotationPointZ = -MathHelper.sin(bipedBody.rotateAngleY) * 5F;
+      bipedLeftArm.rotationPointX = MathHelper.cos(bipedBody.rotateAngleY) * 5F - 2.0F;
       bipedRightArm.rotateAngleY += bipedBody.rotateAngleY;
       bipedLeftArm.rotateAngleY += bipedBody.rotateAngleY;
       bipedLeftArm.rotateAngleX += bipedBody.rotateAngleY;
@@ -197,17 +191,15 @@ public class ModelFairy extends ModelBiped {
       f6 *= f6;
       f6 *= f6;
       f6 = 1.0F - f6;
-      final float f8 = MathHelper.sin(f6 * (float)Math.PI);
-      final float f9 = MathHelper.sin(swingProgress * (float)Math.PI) *
-                       -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
+      final float f8 = MathHelper.sin(f6 * (float) Math.PI);
+      final float f9 = MathHelper.sin(swingProgress * (float) Math.PI) * -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
       bipedRightArm.rotateAngleX -= f8 * 1.2D + f9;
       bipedRightArm.rotateAngleY += bipedBody.rotateAngleY * 2.0F;
-      bipedRightArm.rotateAngleZ =
-          MathHelper.sin(swingProgress * (float)Math.PI) * -0.4F;
+      bipedRightArm.rotateAngleZ = MathHelper.sin(swingProgress * (float) Math.PI) * -0.4F;
     }
 
     if (flymode) {
-      final float f7 = (float)Math.PI;
+      final float f7 = (float) Math.PI;
       bipedBody.rotateAngleX = f7 / 2.0F;
       bipedBody.rotationPointY = 19F;
       wingLeft.rotateAngleX = f7 / 2.0F;
@@ -298,18 +290,22 @@ public class ModelFairy extends ModelBiped {
     wingRight.rotateAngleZ -= Math.cos(sinage) / (flymode ? 3F : 8F);
   }
 
+  @Override
+  public void postRenderArm(float scale, EnumHandSide side) {
+	  FairyFactions.LOGGER.debug("drawing arm overlay");
+    ModelRenderer modelrenderer = this.getArmForSide(side);
+    float f = 0.25F * (float) (side == EnumHandSide.RIGHT ? 1 : -1);
+
+    modelrenderer.rotationPointX += f;
+    modelrenderer.postRender(scale);
+    modelrenderer.rotationPointX -= f;
+  }
   /*
    * NOTE: Apparently removed in 1.8
    *
-  @Override
-      public void renderEars(final float f)
-  {
-  }
-
-  @Override
-      public void renderCloak(final float f)
-  {
-  }
+   * @Override public void renderEars(final float f) { }
+   * 
+   * @Override public void renderCloak(final float f) { }
    */
 
   public ModelRenderer strand, strand2, strand3, strand4;
